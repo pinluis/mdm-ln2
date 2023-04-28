1a7a9f8e86636f387a36ec63b863d7199a9153f4
Luis <luis.pinheiro95@gmail.com>
2023-04-28 18:46:56 +0200

    basic setup

---
+++ src/main/java/ch/zhaw/deeplearningjava/pinheluiln2/PinheluiLn2Application.java
@@ -0,0 +1,13 @@
+package ch.zhaw.deeplearningjava.pinheluiln2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinheluiLn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinheluiLn2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/deeplearningjava/pinheluiln2/PinheluiLn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.deeplearningjava.pinheluiln2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinheluiLn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
