886dd8c5f4aa3fd9efdb0cfd482b76c7ebdc7069
Luis <luis.pinheiro95@gmail.com>
2023-05-02 15:41:22 +0200

    Cleaning code

--- src/main/java/ch/zhaw/deeplearningjava/pinheluiln2/SuperResolution.java
+++ src/main/java/ch/zhaw/deeplearningjava/pinheluiln2/SuperResolution.java
@@ -2,97 +2,31 @@ package ch.zhaw.deeplearningjava.pinheluiln2;
 
 import ai.djl.Application;
 import ai.djl.ModelException;
-import ai.djl.engine.Engine;
+
 import ai.djl.inference.Predictor;
 import ai.djl.modality.cv.Image;
-import ai.djl.modality.cv.ImageFactory;
-import ai.djl.modality.cv.util.NDImageUtils;
-import ai.djl.ndarray.NDArray;
-import ai.djl.ndarray.NDArrays;
-import ai.djl.ndarray.NDList;
-import ai.djl.ndarray.NDManager;
-import ai.djl.ndarray.types.DataType;
+
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public final class SuperResolution {
 
-    private static final Logger logger = LoggerFactory.getLogger(SuperResolution.class);
 
     private SuperResolution() {}
-
-    // public static void main(String[] args) throws ModelException, TranslateException, IOException {
-    //     String imagePath = "src/main/resources/";
-    //     ImageFactory imageFactory = ImageFactory.getInstance();
-
-    //     List<Image> inputImages =
-    //             Collections.singletonList(imageFactory.fromFile(Paths.get(imagePath + "WIN_20230502_14_18_09_Pro.png")));
-
-    //     List<Image> enhancedImages = enhance(inputImages);
-
-    //     logger.info("Using TensorFlow Engine. {} images generated.", enhancedImages.size());
-    //     saveImages(inputImages, enhancedImages);
-    // }
 
     public static Image enhanceSingle(Image inputImage) throws IOException, ModelException, TranslateException {
         List<Image> inputImages = Collections.singletonList(inputImage);
         List<Image> enhancedImages = enhance(inputImages);
         return enhancedImages.get(0);
-    }
-    
-
-    private static void saveImages(List<Image> input, List<Image> generated) throws IOException {
-        Path outputPath = Paths.get("build/output/super-res/");
-        Files.createDirectories(outputPath);
-
-        save(generated, "image", outputPath);
-        save(group(input, generated), "stitch", outputPath);
-
-        logger.info("Generated images have been saved in: {}", outputPath);
-    }
-
-    private static void save(List<Image> images, String name, Path path) throws IOException {
-        for (int i = 0; i < images.size(); i++) {
-            Path imagePath = path.resolve(name + i + ".png");
-            images.get(i).save(Files.newOutputStream(imagePath), "png");
-        }
-    }
-
-    private static List<Image> group(List<Image> input, List<Image> generated) {
-        NDList stitches = new NDList(input.size());
-
-        try (NDManager manager = Engine.getEngine("TensorFlow").newBaseManager()) {
-            for (int i = 0; i < input.size(); i++) {
-                int scale = 4;
-                int width = scale * input.get(i).getWidth();
-                int height = scale * input.get(i).getHeight();
-
-                NDArray left = input.get(i).toNDArray(manager);
-                NDArray right = generated.get(i).toNDArray(manager);
-
-                left = NDImageUtils.resize(left, width, height, Image.Interpolation.BICUBIC);
-                right = right.toType(DataType.FLOAT32, false);
-
-                stitches.add(NDArrays.concat(new NDList(left, right), 1));
-            }
-
-            return stitches.stream()
-                    .map(array -> ImageFactory.getInstance().fromNDArray(array))
-                    .collect(Collectors.toList());
-        }
     }
 
     public static List<Image> enhance(List<Image> inputImages)
