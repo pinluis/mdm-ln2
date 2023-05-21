073c09273c1a7e2d9c2e923154e27d2a7198aae9
Luis <luis.pinheiro95@gmail.com>
2023-05-21 16:40:13 +0200

    Add static frontend

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,40 @@
+document.getElementById("submitButton").addEventListener("click", function () {
+  var fileInput = document.getElementById("fileInput");
+  var file = fileInput.files[0];
+
+  if (!file) {
+    alert("Please select a PNG file before clicking submit.");
+    return;
+  }
+
+  var formData = new FormData();
+  formData.append("file", file);
+
+  // Display the original image
+  var originalImageElement = document.getElementById("originalImage");
+  originalImageElement.src = URL.createObjectURL(file);
+  originalImageElement.style.display = "block";
+
+  // Show loading spinner
+  var loadingSpinner = document.getElementById("loadingSpinner");
+  loadingSpinner.style.display = "block";
+
+  fetch("/enhance", {
+    // Replace with your host name and port
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.blob())
+    .then((imageBlob) => {
+      // Hide loading spinner
+      loadingSpinner.style.display = "none";
+
+      // Display the enhanced image
+      var enhancedImageElement = document.getElementById("resultImage");
+      enhancedImageElement.src = URL.createObjectURL(imageBlob);
+      enhancedImageElement.style.display = "block";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
